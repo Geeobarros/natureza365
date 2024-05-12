ee384ee6df0e12d7665ef8461d34f249b9edf45c
Geeobarros <ge.barros96@gmail.com>
2024-05-12 17:44:44 -0300

    rota post criada

--- src/routes/local.routes.js
+++ src/routes/local.routes.js
@@ -3,6 +3,8 @@ const Local = require("../models/Local");
 const { auth } = require("../middleware/auth");
 const LocalController = require("../controllers/LocalController");
 
+locaisRoutes.post("/", auth, LocalController.cadastrar_local);
+
 locaisRoutes.get("/", auth, LocalController.listarLocais);
 
 module.exports = locaisRoutes;
